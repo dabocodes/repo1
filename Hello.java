0cae50a9cf36f7fda15900f9d1b4cf18868c9cf2
Panbo_win <1040408830@qq.com>
2022-05-22 20:05:18 +0800

    修改 Hello.java 文件中的内容：添加注释

--- Hello.java
+++ Hello.java
@@ -1,5 +1,14 @@
+/**
+ * HelloWorld 演示
+ */
 public class Hello{
+    /**
+     * main，主方法，是Java程序的入口；
+     * 所有，Java程序都是从这里开始执行。
+     * @param args
+     */
     public static void main(String[] args) {
+        // 打印输出hello World
         System.out.println("Hello World");
     }
 }
